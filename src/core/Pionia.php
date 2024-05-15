5883e2622e52b65348566a08817ac2fcb0c7ff22
jet2018 <ezrajet9@gmail.com>
2024-05-15 16:48:29 +0300

    preparing v1.0.4

--- src/core/Pionia.php
+++ src/core/Pionia.php
@@ -16,7 +16,7 @@ class Pionia
 
     public static array | null $settings = null;
 
-    public static string $version = '1.0.3';
+    public static string $version = '1.0.4';
 
     public static string $name = 'Pionia';
 
