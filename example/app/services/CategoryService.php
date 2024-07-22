6c37a3d1fa70b1567698a0db31c9c2f8094a2e26
jet2018 <ezrajet9@gmail.com>
2024-07-22 11:55:29 +0300

    resolved the bug with authentication backends that don't return

--- example/app/services/CategoryService.php
+++ example/app/services/CategoryService.php
@@ -2,10 +2,7 @@
 
 namespace application\services;
 
-use Exception;
 use Pionia\Generics\UniversalGenericService;
-use Pionia\Response\BaseResponse;
-use Porm\Porm;
 
 class CategoryService extends UniversalGenericService
 {
@@ -14,30 +11,4 @@ class CategoryService extends UniversalGenericService
     public ?array $listColumns = ['id', 'name', 'created_at'];
 
     public bool $serviceRequiresAuth = false;
-
-    public function heyWorld()
-    {
-        return BaseResponse::JsonResponse(0, "Hello World");
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function getItem(): ?object
-    {
-        logger->info("Getting item");
-        logger->debug("Getting item");
-        logger->critical("Getting item");
-        logger->error("Getting item");
-        logger->warning("Getting item");
-        logger->notice("Getting item");
-        logger->alert("Getting item");
-        logger->emergency("Getting item");
-        $data = $this->request->getData();
-        $this->requires([$this->pk_field]);
-        $id = $data[$this->pk_field];
-        $item = Porm::from($this->table)->columns($this->listColumns)->get($id);
-        logger->info("Gotten item");
-        return $item;
-    }
 }
