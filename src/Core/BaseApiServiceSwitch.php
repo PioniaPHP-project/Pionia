7505360a8a18afd756bed586196e9e1b414148a5
jet2018 <ezrajet9@gmail.com>
2024-07-26 15:50:28 +0300

    updated the core docs to reflect the new functionality

--- src/Core/BaseApiServiceSwitch.php
+++ src/Core/BaseApiServiceSwitch.php
@@ -28,8 +28,11 @@ abstract class BaseApiServiceSwitch
      * This method must be implemented by the child class to return an array of services.
      *
      * The array should be in the format of `['SERVICE_NAME' => new SERVICE_CLASS()]`.
-     * The `SERVICE_NAME` is the key that will be used to call the service.
-     * The `SERVICE_CLASS` is the class that will be called when the SERVICE_NAME is called.
+     * The SERVICE_NAME is the name that will be used in the request data to call the service.
+     * The SERVICE_CLASS is the class that will be called when the SERVICE_NAME is called.
+     * The SERVICE_CLASS must extend the BaseRestService class.
+     * @version 1.1.7 array can be in the format of `['SERVICE_NAME' => SERVICE_CLASS::class]`.
+
      *
      * @example
      * ```php
@@ -39,6 +42,14 @@ abstract class BaseApiServiceSwitch
      *       'service1' => new Service1(),
      *       'service2' => new Service2(),
      *     ];
+     * }
+     * // version 1.1.7 and above, you can also do the following
+     * public function registerServices() :array
+     * {
+     *   return [
+     *     'service1' => Service1::class,
+     *    'service2' => Service2::class,
+     *   ];
      * }
      * ```
      * @return array The array of services
